52bfdc506ce1eb16329a45c49560a6b5b74c23cc
Adam Byrne <adam.r.byrne@gmail.com>
2020-06-18 23:35:17 +0100

    refactor

--- app.py
+++ app.py
@@ -1,103 +1,106 @@
-
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, session, render_template, request, redirect, send_file
 import base64
 from PIL import Image
 import numpy as np
 import cv2
+import random
+import string
+
 
 app = Flask(__name__)
-images = []
-imageObjects = []
-final = []
-frames = []
-sizes = []
+pillows = []
 
-def done():
-    images = []
-    imageObjects = []
-    final = []
-    frames = []
-    sizes = []
 
-def formImage(string):
-    image_b64 = string.split(",")[1]
-    binary = base64.b64decode(image_b64)
-    image = np.asarray(bytearray(binary), dtype="uint8")
-    x = cv2.imdecode(image, cv2.IMREAD_COLOR)
-    image =  Image.fromarray(x)
-    return image
+class Img:
+	def __init__(self, base, header, filename, ctype):
+		self.base = base
+		self.header = header
+		self.uri = ("data:" + self.header + ";" + "base64," + self.base)
+		self.filename = filename
+		self.type = ctype
 
-def reshape(i):
-    n =""
-    width, height = i.size
-    if width > 600 or height > 300:
-        left = (width/2) - 300
-        right = (width/2) + 300
-        top = (height/2) -150
-        bottom = (height/2) +150 
-        n = i.crop((left, top, right, bottom))
-    else:
-        return i 
-    return n
+	def size(self, image):
+		width, height = image.size
+		self.dimensions = image.size
+		return (width, height)
 
-def fixColor(imgs):
-    nimgs = []
-    for i in imgs:
-        nimgs.append(cv2.cvtColor(np.array(reshape(i)), cv2.COLOR_BGR2RGB))
-    return nimgs
+	def dimensions(self):
+		k = self.formImage()
+		return self.size(k)
+
+	def reshape(self, frame):
+		n = ""
+		width, height = self.size(frame)
+		if width > 600 or height > 300:
+			left = (width/2) - 300
+			right = (width/2) + 300
+			top = (height/2) - 150
+			bottom = (height/2) + 150
+			n = frame.crop((left, top, right, bottom))
+		else:
+			return frame
+		return n
+
+	def fixColor(self, i):
+		return cv2.cvtColor(i, cv2.COLOR_BGR2RGB)
+
+	def formImage(self):
+		image_b64 = self.uri.split(",")[1]
+		binary = base64.b64decode(image_b64)
+		image = np.asarray(bytearray(binary), dtype="uint8")
+		x = cv2.imdecode(image, cv2.IMREAD_COLOR)
+		x = self.fixColor(x)
+		frame = Image.fromarray(x)
+		k = self.reshape(frame)
+		return k
+
+	def retpil(self):
+		return self.formImage()
+
+
+def reorder(data, rank):
+  new = []
+  count = 0
+  for o in rank:
+    new.insert(int(o), data[count])
+    count += 1
+
+  return new
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        uploadedFiles = request.files.getlist("images")
-        for upload in uploadedFiles:
-            '''
-            buffer 0 > Header
-            buffer 1 > Filename
-            buffer 2 > URI
-            buffer 3 > base64 raw
+	if request.method == "POST":
+		uploadedFiles = request.files.getlist("images")
+		images = []
+		for upload in uploadedFiles:
+			read = upload.read()
+			image = Img(base64.b64encode(read).decode(
+			), upload.headers['Content-Type'], upload.filename, upload.content_type)
 
-            '''
-            buffer = []
-            read = upload.read()
-            raw = base64.b64encode(read).decode()
-            buffer.append(upload.content_type)
-            buffer.append(upload.filename)
-            buffer.append(("data:" + upload.headers['Content-Type'] + ";" + "base64," + raw))
-            buffer.append(raw)
-            images.append(buffer)
-            imageObjects.append(formImage(buffer[2]))
-            width, height = formImage(buffer[2]).size
-            sizes.append(f"{width}px x {height}px")
-            final = fixColor(imageObjects)
-            for i in final:
-                img = Image.fromarray(i, 'RGB')
-                frames.append(img)
-        return render_template("index.html", images=images, sizes=sizes)
-    else:
-        done()
-        return render_template("index.html")
+			obj = image.retpil()
+			pillows.append(obj)
+			images.append(image)
 
-@app.route("/reorder", methods=["GET", "POST"])
-def reorder():
-    if request.method == "POST":
-        res = [i for n, i in enumerate(frames) if i not in frames[:n]] 
-        df = int(request.form["fps"])
-        order = []
-        newFrames = []
-        for i in range(len(res)):
-            newFrames.append(0)
-        for k in request.form.getlist("rank"):
-            order.append(k)
-        for i in order:
-            newFrames.append(res[int(i)])
-        del newFrames[:2]
+		return render_template("index.html", imgs=images)
+	else:
 
-        newFrames[0].save("result.gif", format='GIF', append_images=newFrames[1:], save_all=True, duration=200, loop=0)
-        return send_file("result.gif", as_attachment=True)
-    else:
-        return redirect("/")
+		return render_template("index.html")
+
+
+@app.route("/make", methods=["GET", "POST"])
+def make():
+	if request.method == "POST":
+		duration = int(request.form["fps"])
+		newOrder = request.form.getlist("rank")
+
+		pox = reorder(pillows, newOrder)
+
+		pox[0].save("result.gif", format='GIF', append_images=pox[1:], save_all=True, duration=duration, loop=0)
+		return send_file("result.gif", as_attachment=True)
+	else:
+		return redirect("/")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+	app.run(debug=True)
